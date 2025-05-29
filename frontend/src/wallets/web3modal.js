f61868024bd26ca938dfa119fadead297168835c
Matias Benary <matiasbenary@gmail.com>
2025-05-29 17:49:04 -0300

    fix(pnpm): resolve module installation error

--- frontend/src/wallets/web3modal.js
+++ frontend/src/wallets/web3modal.js
@@ -6,11 +6,30 @@ import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 // Get your projectId at https://cloud.reown.com
 const projectId = "5bb0fe33763b3bea40b8d69e4269b4ae";
 
+const connectors = [
+  walletConnect({
+    projectId,
+    metadata: {
+      name: "Donation",
+      description: "Examples demonstrating integrations with NEAR blockchain",
+      url: "https://near.github.io/wallet-selector",
+      icons: ["https://near.github.io/wallet-selector/favicon.ico"],
+    },
+    showQrModal: false, // showQrModal must be false
+  }),
+  injected({ shimDisconnect: true }),
+];
+
 export const wagmiAdapter = new WagmiAdapter({
   projectId,
+  connectors,
   networks: [nearTestnet],
 });
 
+// Preserve login state on page reload
+reconnect(wagmiAdapter.wagmiConfig);
+
+// Modal for login
 export const web3Modal = createAppKit({
   adapters: [wagmiAdapter],
   projectId,
